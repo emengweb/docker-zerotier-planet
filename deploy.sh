1334159fce9954a4f9532b995316a42b290b68e2
emengweb <31469739+emengweb@users.noreply.github.com>
2022-05-13 14:17:07 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ docker build --network host -t $imageName .
 echo "启动服务"
 for i in $(lsof -i:9993 -t);do kill -2 $i;done
 #docker run -d --network host --name $imageName --restart unless-stopped $imageName
-docker run -d -p9993:9993/udp -p127.0.0.1:3000:3000 \
+docker run -d -p9993:9993 -p127.0.0.1:3000:3000 \
 #    -v $PWD/ztncui:/opt/key-networks/ztncui/src/etc \
     -v $PWD/zt1:/var/lib/zerotier-one \
     --name $imageName --restart unless-stopped $imageName
