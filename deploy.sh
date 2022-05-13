a8eef50883b273e2035a72c6ac3bdc098ba20a9a
emengweb <31469739+emengweb@users.noreply.github.com>
2022-05-13 14:26:49 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ echo "启动服务"
 for i in $(lsof -i:9993 -t);do kill -2 $i;done
 #docker run -d --network host --name $imageName --restart unless-stopped $imageName
 docker run -d -p9993:9993 -p127.0.0.1:3000:3000 \
-#    -v $PWD/ztncui:/opt/key-networks/ztncui/src/etc \
     -v $PWD/zt1:/var/lib/zerotier-one \
     --name $imageName --restart unless-stopped $imageName
+#    -v $PWD/ztncui:/opt/key-networks/ztncui/src/etc \
 docker cp zerotier-planet:/app/bin/planet ./planet
