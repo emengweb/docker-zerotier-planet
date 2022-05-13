b1ef6dec20d6d41dcaf434899608689a87c028fc
emengweb <31469739+emengweb@users.noreply.github.com>
2022-05-13 13:42:07 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ docker run -d -p9993:9993/udp -p127.0.0.1:3000:3000 \
     -v $PWD/zt1:/var/lib/zerotier-one \
     -v $PWD/bin/:/app/bin \
     --name $imageName --restart unless-stopped $imageName
-#docker cp zerotier-planet:/app/bin/planet /opt/planet
+docker cp zerotier-planet:/app/bin/planet ./planet
