5a8548a938b54834c60a8d5266184c15057f7f8d
emengweb <31469739+emengweb@users.noreply.github.com>
2022-05-13 13:18:39 +0800

    PWD

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ echo "启动服务"
 for i in $(lsof -i:9993 -t);do kill -2 $i;done
 #docker run -d --network host --name $imageName --restart unless-stopped $imageName
 docker run -d -p9993:9993/udp -p127.0.0.1:3000:3000 \
-    -v ./ztncui:/opt/key-networks/ztncui/etc \
-    -v ./zt1:/var/lib/zerotier-one \
-    -v ./bin/:/app/bin/ \
+    -v $PWD/ztncui:/opt/key-networks/ztncui/etc \
+    -v $PWD/zt1:/var/lib/zerotier-one \
+    -v $PWD/bin/:/app/bin/ \
     --name $imageName --restart unless-stopped $imageName
 #docker cp zerotier-planet:/app/bin/planet /opt/planet
